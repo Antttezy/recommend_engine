7112dff2e6a91823a3150740924329092e10ddca
Antttezy <antkumachev@gmail.com>
2026-04-17 02:06:30 +0300

    Add workaround for non-local grpc import

--- api/grpc/compile.sh
+++ api/grpc/compile.sh
@@ -6,3 +6,5 @@ GRPC_OUT=api/grpc/vector_processor
 python -m grpc_tools.protoc \
 -I$GRPC_IN --python_out=$GRPC_OUT --pyi_out=$GRPC_OUT --grpc_python_out=$GRPC_OUT \
 vector_processor.proto
+
+sed -i "s/^import \(.*_pb2\)/from . import \1/" api/grpc/vector_processor/vector_processor_pb2_grpc.py
