767b8e0ebbd54efc59e1e91c249fe83e0a01c5a5
Antttezy <antkumachev@gmail.com>
2026-04-16 21:17:48 +0300

    Add embedding type checks

--- core/models/embedding.py
+++ core/models/embedding.py
@@ -1,7 +1,20 @@
 from dataclasses import dataclass
+from core.const import EMBEDDING_LENGTH
 
 
 @dataclass
 class Embedding:
     """Represents an embedding vector"""
     data: list[float]
+
+    def __post_init__(self):
+        if not isinstance(self.data, list):
+            raise TypeError("Embedding.data must be a list")
+
+        if len(self.data) != EMBEDDING_LENGTH:
+            raise ValueError(
+                f"Embeddings must be {EMBEDDING_LENGTH}-dimentional, got {len(self.data)}"
+            )
+
+        if not all(isinstance(x, (float, int)) for x in self.data):
+            raise TypeError("Embedding must contain only numeric values")
