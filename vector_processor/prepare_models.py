9d6c18bdc68f256b9dfbe956b48bce40cd2bb04d
Antttezy <antkumachev@gmail.com>
2026-04-17 13:12:12 +0300

    Add entrypoint for downloading models to cache

---
+++ vector_processor/prepare_models.py
@@ -0,0 +1,27 @@
+import asyncio
+import logging
+import os
+
+from vector_processor import config
+
+
+async def main():
+    from transformers import CLIPModel, CLIPProcessor
+
+    # Config and logging
+    settings = config.load_config()
+    logging.basicConfig(level=settings.LOG_LEVEL)
+
+    # CLIP device
+    ml_device = "cpu"
+
+    # Download models to cache
+    CLIPModel.from_pretrained(settings.CLIP_MODEL).to(ml_device)
+    CLIPProcessor.from_pretrained(settings.CLIP_PROCESSOR)
+
+
+if __name__ == "__main__":
+    # Unset variables triggering strict offline mode
+    os.environ.pop("TRANSFORMERS_OFFLINE", None)
+    os.environ.pop("HF_DATASETS_OFFLINE", None)
+    asyncio.run(main())
